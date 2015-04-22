5ef3404979f28a75fd90d813cbc91b0a932bef20
Tinsulate <siira.jussi@gmail.com>
2015-04-22 13:20:33 +0300

    Implemented basic drag and drop using https://github.com/logicbomb/lvlDragDrop

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'lvl.directives.dragdrop'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -18,7 +18,29 @@ angular.module('myApp.view1', ['ngRoute'])
         sc.items.push(fl('date')(newDate, "short"));
       }
 
-    }]);
+    sc.dropped = function(dragEl, dropEl) { // function referenced by the drop target
+        //this is application logic, for the demo we just want to color the grid squares
+        //the directive provides a native dom object, wrap with jqlite
+        var drop = angular.element(dropEl);
+        var drag = angular.element(dragEl);
+
+        //clear the previously applied color, if it exists
+        var bgClass = drop.attr('data-color');
+        if (bgClass) {
+            drop.removeClass(bgClass);
+        }
+
+        //add the dragged color
+        bgClass = drag.attr("data-color");
+        drop.addClass(bgClass);
+        drop.attr('data-color', bgClass);
+
+        //if element has been dragged from the grid, clear dragged color
+        if (drag.attr("x-lvl-drop-target")) {
+            drag.removeClass(bgClass);
+        }
+    }
+}]);
 
 
 
